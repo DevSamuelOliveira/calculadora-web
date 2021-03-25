4838cd23e859128f2f6f28786ec382da53617b71
Samuel Oliveira <samuel.virtuoso@hotmail.com>
2021-03-25 20:49:08 -0300

    Criando arquivo js para capturar teclas e fazendo estrutura básica para a ação

---
+++ js/teclas.js
@@ -0,0 +1,40 @@
+document.onkeydown = escrever;
+
+function escrever(key){
+  if (key.keyCode === 48){
+
+  }
+  if (key.keyCode === 49){
+
+  }
+  if (key.keyCode === 50){
+
+  }
+  if (key.keyCode === 51){
+
+  }
+  if (key.keyCode === 52){
+
+  }
+  if (key.keyCode === 53){
+
+  }
+  if (key.keyCode === 54){
+
+  }
+  if (key.keyCode === 55){
+
+  }
+  if (key.keyCode === 56){
+
+  }
+  if (key.keyCode === 57){
+
+  }
+  if (key.keyCode === 58){
+
+  }
+  if (key.keyCode === 59){
+
+  }
+}
