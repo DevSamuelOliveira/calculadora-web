9f37b77acc1fb2bb1e45a9ee173edc31f24465f9
Samuel Oliveira <samuel.virtuoso@hotmail.com>
2021-03-25 16:53:12 -0300

    Alteração de titulo, tamanhos, adicionando novos botões verificação da quantidade de números para adicionar . e ajustes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,108 @@
-const buttons = document.querySelectorAll('#table-calc button')
+function checkSizeNumbers(){
+  if(buttons[0].cells[0].innerText.length != 15){
+    if(buttons[0].cells[0].innerText.length == 3){
+      let troca = buttons[0].cells[0].innerText
+      troca = troca[0] + '.' + troca[1] + troca.slice(2, troca.length)
+      buttons[0].cells[0].innerText = troca
+    }
+    return true
+  }
+}
 
-buttons[1].addEventListener('click', () => {
-  buttons[0].innerText = buttons[1].innerText
+var buttons = document.getElementById("table-calc").rows
+
+buttons[1].cells[0].addEventListener("click", ()=>{
+  if (checkSizeNumbers() == true){
+    buttons[0].cells[0].innerText += buttons[1].cells[0].innerText
+  }
+})
+
+buttons[1].cells[1].addEventListener("click", ()=>{
+  if (checkSizeNumbers() == true){
+    buttons[0].cells[0].innerText += buttons[1].cells[1].innerText
+  }
+})
+
+buttons[1].cells[2].addEventListener("click", ()=>{
+  if (checkSizeNumbers() == true){
+    buttons[0].cells[0].innerText += buttons[1].cells[2].innerText
+  }
+})
+
+buttons[1].cells[3].addEventListener("click", ()=>{
+  if (checkSizeNumbers() == true){
+    buttons[0].cells[0].innerText += buttons[1].cells[3].innerText
+  }
+})
+
+buttons[2].cells[0].addEventListener("click", ()=>{
+  if (checkSizeNumbers() == true){
+    buttons[0].cells[0].innerText += buttons[2].cells[0].innerText
+  }
+})
+
+buttons[2].cells[1].addEventListener("click", ()=>{
+  if (checkSizeNumbers() == true){
+    buttons[0].cells[0].innerText += buttons[2].cells[1].innerText
+  }
+})
+
+buttons[2].cells[2].addEventListener("click", ()=>{
+  if (checkSizeNumbers() == true){
+    buttons[0].cells[0].innerText += buttons[2].cells[2].innerText
+  }
+})
+
+buttons[2].cells[3].addEventListener("click", ()=>{
+  if (checkSizeNumbers() == true){
+    buttons[0].cells[0].innerText += buttons[2].cells[3].innerText
+  }
+})
+
+buttons[3].cells[0].addEventListener("click", ()=>{
+  if (checkSizeNumbers() == true){
+    buttons[0].cells[0].innerText += buttons[3].cells[0].innerText
+  }
+})
+
+buttons[3].cells[1].addEventListener("click", ()=>{
+  if (checkSizeNumbers() == true){
+    buttons[0].cells[0].innerText += buttons[3].cells[1].innerText
+  }
+})
+
+buttons[3].cells[2].addEventListener("click", ()=>{
+  if (checkSizeNumbers() == true){
+    buttons[0].cells[0].innerText += buttons[3].cells[2].innerText
+  }
+})
+
+buttons[3].cells[3].addEventListener("click", ()=>{
+  if (checkSizeNumbers() == true){
+    buttons[0].cells[0].innerText += buttons[3].cells[3].innerText
+  }
+})
+
+buttons[4].cells[0].addEventListener("click", ()=>{
+  if (checkSizeNumbers() == true){
+    buttons[0].cells[0].innerText += buttons[4].cells[0].innerText
+  }
+})
+
+buttons[4].cells[1].addEventListener("click", ()=>{
+  if (checkSizeNumbers() == true){
+    buttons[0].cells[0].innerText += buttons[4].cells[1].innerText
+  }
+})
+
+buttons[4].cells[2].addEventListener("click", ()=>{
+  if (checkSizeNumbers() == true){
+    buttons[0].cells[0].innerText += buttons[4].cells[2].innerText
+  }
+})
+
+buttons[4].cells[3].addEventListener("click", ()=>{
+  if (checkSizeNumbers() == true){
+    buttons[0].cells[0].innerText += buttons[4].cells[3].innerText
+  }
 })
